bf7a2470125531ffb2083a42755e0ca919f17c1a
Jose Medina <fermed28@gmail.com>
2019-04-25 20:37:30 -0500

    simple apivotes

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json:@users.as_json(include: [:votes])
+    end
+
+    def update
+        user = User.find(params[id])
+        user.votes.create
+        render json:user.as_json(include: [:votes])
+    end
+
+    def delete
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.find_or_create_by({name: 'liliana'})
+User.find_or_create_by({name:'andres'})
+User.find_or_create_by({name: 'oscar'})
+User.find_or_create_by({name:'julian'})
+User.find_or_create_by({name: 'hugo'})
+User.find_or_create_by({name:'alejandro'})
+User.find_or_create_by({name:'jorge'})
+User.find_or_create_by({name:'cindy'})
+User.find_or_create_by({name:'ramiro'})
+User.find_or_create_by({name:'Angie'})
+User.find_or_create_by({name:'Ferney'})
