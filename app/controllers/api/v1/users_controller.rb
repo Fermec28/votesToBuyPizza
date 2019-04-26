6e477869a926f08934d8a9a19863b930ddb06a40
Jose Medina <fermed28@gmail.com>
2019-04-26 15:42:36 -0500

    [fixes] add put on permitted request and remove proteccion with forgery cross domain

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def update
-        user = User.find(params[id])
+        user = User.find(params[:id])
         user.votes.create
         render json:user.as_json(include: [:votes])
     end
